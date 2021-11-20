c0bad09f3c4a7e54fde3d6f2ff013da8a584d2d7
Kornings <korningss@gmail.com>
2021-11-20 23:50:12 +0700

    first commit

---
+++ js/onscroll.js
@@ -0,0 +1,8 @@
+window.onload = function () { scrollFunction() };
+
+function scrollFunction() {
+    var navbar = document.getElementById("navbar");
+        navbar.classList.add("shadow");
+        navbar.classList.add("bg-white");
+        navbar.classList.remove("mt-3");
+}
